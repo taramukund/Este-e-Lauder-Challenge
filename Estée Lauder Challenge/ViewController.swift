63ebbb45d28a09b6870a96dc216a228c15ecc81e
taramukund <tara.mukund1@gmail.com>
2021-07-23 08:08:40 +0900

    Initial Commit

---
+++ Estée Lauder Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Estée Lauder Challenge
+//
+//  Created by Tara on 23/07/21.
+//  Copyright © 2021 KWK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Estée Lauder ChallengeTests/Este_e_Lauder_ChallengeTests.swift
@@ -0,0 +1,34 @@
+//
+//  Este_e_Lauder_ChallengeTests.swift
+//  Estée Lauder ChallengeTests
+//
+//  Created by Tara on 23/07/21.
+//  Copyright © 2021 KWK. All rights reserved.
+//
+
+import XCTest
+@testable import Este_e_Lauder_Challenge
+
+class Este_e_Lauder_ChallengeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
